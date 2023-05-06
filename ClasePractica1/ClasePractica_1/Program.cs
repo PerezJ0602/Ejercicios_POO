82c55a04e7fd739e1a6e6c8eb425183ebe1f4073
PerezJ0602 <86813446+PerezJ0602@users.noreply.github.com>
2023-05-05 22:58:18 -0600

    Add files via upload
    Ejercicio1

---
+++ ClasePractica1/ClasePractica_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasePractica_1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instanciar clase vehivulo
+            Vehiculo veh = new Vehiculo(1,"Nissan","Aveo",5000,15000,2017);
+            Console.WriteLine(veh.ToString());
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
